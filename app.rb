7cd9ff70088dce99554efc83fd6bb8e7ed403648
administrator <administrator@administrators-iMac.local>
2018-05-18 10:27:50 +0800

    first commit

---
+++ app.rb
@@ -0,0 +1,28 @@
+require("bundler")
+Bundler.require
+
+get "/" do
+  erb :index
+end
+
+def worksheet
+  @session ||= GoogleDrive::Session.from_service_account_key("client_secret.json")
+  @spreadsheet ||= @session.spreadsheet_by_title("Gangga's spreadsheet")
+  @worksheet ||= @spreadsheet.worksheets.first
+end
+
+post "/" do
+  new_row = [params["nameofsupplier"], params["country"], params["webadd"], params["emailadd"], params["emailadd2"], params["contactnum1"], params["contactnum2"],params["contactperson"], params["productcat"], params["businesscard"], params["brochurecat"], params["productleadshiptime"], params["shippingcost"], params["smallorder(num)"], params["unit price for small order"], params["bulkorder"], params["unit price for bulk order"], params["productsample"]]
+  begin
+    worksheet.insert_rows(worksheet.num_rows+1, [new_row])
+    worksheet.save
+    erb :thanks
+  rescue
+    erb :index, locals: {
+      error_mesage: "Your details could not be saved, please try again!"
+    }
+  end
+end
+
+
+  # params["country"], params["webadd"], params["emailadd"], params["emailadd2"], params["contactnum1"], params["contactnum2"], params["productcat"], params["businesscard"], params["brochurecat"], params["productleadshiptime"], params["shippingcost"], params["smallorder(num)"], params["unit price for small order"], params["bulkorder"], params["unit price for bulk order"], params["productsample"]]
