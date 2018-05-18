291401d8dc040c33d99a68c80d04b2f44ec81090
administrator <administrator@administrators-iMac.local>
2018-05-18 18:22:58 +0800

    debugging

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ def worksheet
 end
 
 post "/" do
-  new_row = [params["nameofsupplier"], params["country"], params["webadd"], params["emailadd"], params["emailadd2"], params["contactnum1"], params["contactnum2"],params["contactperson"], params["productcat"], params["businesscard"], params["brochurecat"], params["productleadshiptime"], params["shippingcost"], params["smallorder(num)"], params["unit price for small order"], params["bulkorder"], params["unit price for bulk order"], params["productsample"]]
+  new_row = [params["nameofsupplier"], params["country"], params["webadd"], params["emailadd"], params["emailadd2"], params["contactnum1"], params["contactnum2"],params["contactperson"], params["productcat"], params["businesscard"], params["brochurecat"], params["productleadshiptime"], params["shippingmethod"], params["shippingcost"], params["smallordernum"], params["unitpriceforsmallorder"], params["bulkorder"], params["unitpriceforbulkorder"], params["productsample"], params["productsample"], params["samplingcost"], params["paymentTerms"], params["PracticalComp"], params["PosCons"]]
   begin
     worksheet.insert_rows(worksheet.num_rows+1, [new_row])
     worksheet.save
